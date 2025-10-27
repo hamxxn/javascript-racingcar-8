9319ff9d7292bce2dd0727d4adc66988263635ab
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-27 12:19:28 +0900

    feat(App): GameMannger 인스턴스 생성 및 게임 시작 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import GameManager from "./controller/GameManager.js";
+
 class App {
-  async run() {}
+  constructor() {
+    this.gameManager = new GameManager();
+  }
+
+  async run() {
+    await this.gameManager.playGame();
+  }
 }
 
 export default App;
