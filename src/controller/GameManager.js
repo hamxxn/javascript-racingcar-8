77a40b8601722ad043e424e26b0474233cee0906
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-27 12:14:33 +0900

    feat(controller): 사용자의 입력을 받아 유효성 검사 후 Car 인스턴스를 생성하는 함수 구현
    - createCars(): 이름 배열로 Car 배열 생성
    - getValidatedInputs(): 입력 수집 → 검증 → 파싱

--- src/controller/GameManager.js
+++ src/controller/GameManager.js
@@ -1,9 +1,36 @@
+import Car from "../model/Car.js";
 import { InputView, ResultView } from "../view/index.js";
+import {
+  isValidateCarNames,
+  isValidateRound,
+  parseCarNames,
+} from "../utils/index.js";
 
 class GameManager {
   constructor() {
     this.inputView = new InputView();
     this.resultView = new ResultView();
+  }
+
+  createCars(carNames) {
+    return carNames.map((name) => new Car(name));
+  }
+
+  async playGame() {
+    const { carNames, round } = await this.getValidatedInputs();
+    const cars = this.createCars(carNames);
+  }
+
+  async getValidatedInputs() {
+    const carNamesInput = await this.inputView.readCarNames();
+    isValidateCarNames(carNamesInput);
+    const carNames = parseCarNames(carNamesInput);
+
+    const roundInput = await this.inputView.readRound();
+    isValidateRound(roundInput);
+    const round = parseInt(roundInput);
+
+    return { carNames, round };
   }
 }
 
