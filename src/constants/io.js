36a77fa75f42675dfcc2c01e8da3776e8420c21d
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-27 12:00:24 +0900

    feat(constants): 사용자에게 보여질 io message 상수 구현

---
+++ src/constants/io.js
@@ -0,0 +1,8 @@
+import { CAR_NAME_DELIMITER } from "./car.js";
+
+export const IO_MESSAGES = {
+  CAR_NAMES_MESSAGE: `경주할 자동차 이름을 입력하세요.(이름은 ${CAR_NAME_DELIMITER.LABEL}(${CAR_NAME_DELIMITER.VALUE}) 기준으로 구분)\n`,
+  ROUND_MESSAGE: "시도할 횟수는 몇 회인가요?\n",
+  RESULT_MESSAGE: "실행 결과",
+  FINAL_WINNER_MESSAGE: "최종 우승자 : ",
+};
