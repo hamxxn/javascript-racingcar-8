f317ba7315dd74af149ff0e9e5bd0ab00ed5babb
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-27 12:02:17 +0900

    feat(constants): car 관련 상수 구현
    - 전진 가능한 최소 랜덤 값
    - 이름의 최소/ 최대 길이
    - 랜덤 numer 범위
    - 이름 구분자
    - 전진 표시 문자

---
+++ src/constants/car.js
@@ -0,0 +1,20 @@
+export const CAR_MOVE_CONDITION = {
+  MIN_VALUE: 4,
+};
+
+export const CAR_NAME_LENGTH = {
+  MIN: 1,
+  MAX: 5,
+};
+
+export const RANDOM_NUMBER_RANGE = {
+  MIN: 0,
+  MAX: 9,
+};
+
+export const CAR_NAME_DELIMITER = {
+  LABEL: "쉼표",
+  VALUE: ",",
+};
+
+export const CAR_POSITION_SYMBOL = "-";
